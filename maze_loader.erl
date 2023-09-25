451840a988d5e31ccbb6ff18a93ea79efb8c7b83
Marko Perkusic <marko1perkusic@gmail.com>
2023-09-25 11:42:29 +0200

    Add default path for map loading function

--- maze_loader.erl
+++ maze_loader.erl
@@ -3,9 +3,13 @@
 -export([load/0]).
 
 load() ->
-  io:format("Enter the path of the file: ~n"),
-  File = string:strip(io:get_line(""), right, $\n),
-  {ok, BinaryData} = file:read_file(File),
+  File = string:strip(io:get_line("Enter the path of the file: ~n"), right, $\n),
+  {ok, BinaryData} = case File of
+    [] ->
+      file:read_file("maps/test_map.txt");
+    _ ->
+      file:read_file(File)
+  end,
   Contents = binary_to_list(BinaryData),
   New = string:tokens(Contents, "\n"),
   New.
